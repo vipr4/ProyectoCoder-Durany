f8c427b33e66b7b4852f923b59d8efbce1f12b6d
Valen Durany <valendurany9@gmail.com>
2023-01-10 16:19:55 -0300

    Primeros pasos con java

---
+++ index.js
@@ -0,0 +1,27 @@
+
+//Ingreso a nuestra página web
+
+let nombre_usuario = prompt( "Ingrese su nombre ");
+let edad_usuario = prompt( "Ingresa tu edad: ");
+
+// El usuario no podrá ingresar a la página si no es mayor a 12 años
+
+if( edad_usuario >= 12 ){
+
+    console.log( nombre_usuario + " ha ingresado a nuestra página.." );
+    alert( "Bienvenido a Diamond-s, " + nombre_usuario );
+}
+
+else if( edad_usuario < 12){
+
+    console.log( "El usuario es menor a 12 años ");
+    alert( "Lo sentimos, para ingresar debes ser mayor a 12 años." );
+    let edad_usuario = prompt( "Ingresa tu edad: ");
+
+    if( edad_usuario >= 12){
+
+        alert( "Bienvenido a Diamond-s!" );
+    }
+    console.log( nombre_usuario + " ha ingresado a nuestra página.." );
+}
+
