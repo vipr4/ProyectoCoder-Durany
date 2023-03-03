f0f709667fb9548936b8495ccc7d9fa472baaef8
Valen Durany <valendurany9@gmail.com>
2023-03-03 16:14:38 -0300

    cambios generales

--- index.js
+++ index.js
@@ -5,7 +5,43 @@ let presentacion = document.getElementById("img_presentacion");
 
 presentacion.addEventListener("mouseover", function(){
 
-    img.src = "presentacion-2.png";
+    presentacion.src = "./imagenes-2/presentacion-2.png";
 })
+
+presentacion.addEventListener("mouseout", function(){
+
+    presentacion.src = "./imagenes-2/teconologia-presentacion.jpg";
+})
+
+
+//Evento de carrito
+
+function agregar_al_carrito(e){
+
+    console.log("Se clickeo el boton: ", + e.target);
+
+    let hijo = e.target;
+    let padre = hijo.parentNode;
+    let abuelo = padre.parentNode;
+
+    let nombre_producto = padre.querySelector("h5").textContent;
+
+    console.log(nombre_producto);
+}
+
+let btn_carrito = document.querySelectorAll(".btnAuris");
+
+console.log(btn_carrito);
+
+for ( let boton of btn_carrito){
+
+    boton.addEventListener("click", agregar_al_carrito);
+
+}
+
+
+
+
+
 
 
